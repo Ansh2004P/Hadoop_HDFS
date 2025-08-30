4a460b07d4f04d23f9b66d6b99389d05d681730e
Ansh2004P <anshkpatel15@gmail.com>
2025-08-30 23:24:40 +0530

    Day 01

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Ansh2004P/hdfs/p2p"
+)
+
+func main() {
+	tr := p2p.NewTCPTransport(":3000")
+
+	if err := tr.ListenAndAccept(); err != nil {
+		log.Fatal(err)
+	}
+
+	select {}
+}
