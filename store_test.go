3554af8b5d7a8fd70492c53a841f6ec052ed6e0a
Ansh2004P <anshkpatel15@gmail.com>
2025-09-14 22:58:09 +0530

    Deletion of file part completed

--- store_test.go
+++ store_test.go
@@ -9,21 +9,20 @@ import (
 
 func TestPathTransformFunc(t *testing.T) {
 	key := "momsbestpicture"
-	pathKey := CASPathTransformFun(key)
+	pathKey := CASPathTransformFunc(key)
 	expectedPathName := "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff"
 
 	if pathKey.Pathname != expectedPathName {
 		t.Errorf("have %s want %s", pathKey.Pathname, expectedPathName)
 	}
 }
-
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc: CASPathTransformFun,
+		PathTransformFunc: CASPathTransformFunc,
 	}
 
 	s := NewStore(opts)
-	key := "momsspecials"
+	key := "momsbestpicture"
 	data := []byte("some jpg bytes")
 	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
 		t.Error(err)
@@ -44,5 +43,24 @@ func TestStore(t *testing.T) {
 	if string(b) != string(data) {
 		t.Errorf("want %s have %s", string(data), string(b))
 	}
-	
+
 }
+
+func TestDelete(t *testing.T) {
+	opts := StoreOpts{
+		PathTransformFunc: CASPathTransformFunc,
+	}
+
+	s := NewStore(opts)
+	key :="momsbestpicture";
+	data := []byte("some jpg bytes")
+
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+		t.Error(err)
+	}
+
+	if err := s.Delete(key); err != nil {
+		t.Error(err)
+	}
+}
+
